c351beace6d132a9e1a7199fedfd94870b61b9f7
alesbesdria <melissa@meirsman.fr>
2016-04-24 13:08:59 +0200

    gnl a jour

--- map_read.c
+++ map_read.c
@@ -7,8 +7,11 @@ void		map_read()
 	int		x;
 	int		y;
 	int		z;
+	int		i;
 
-	ft_strsplit(**line, " ");
+	tab = malloc();
 	get_next_line(fd, **line);
+	ft_atoi(tab[i]);
+	ft_strsplit(**line, " ");
 }
 
